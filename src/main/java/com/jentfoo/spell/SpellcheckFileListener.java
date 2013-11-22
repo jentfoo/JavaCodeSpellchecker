ea5c3729e4cb1b24b81e1a6540b8918f16b8b9f6
Mike Jensen <jentfoo@gmail.com>
2013-11-22 08:50:22 -0700

    More implementation, and including libraries (which I don't normally do, but this will be easiest if it is just compiled/ran from eclipse, since this depends on another project which is not published into maven)

--- src/main/java/com/jentfoo/spell/SpellcheckFileListener.java
+++ src/main/java/com/jentfoo/spell/SpellcheckFileListener.java
@@ -4,10 +4,17 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.StringTokenizer;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.languagetool.JLanguageTool;
+import org.languagetool.language.English;
+import org.languagetool.rules.RuleMatch;
 import org.threadly.util.ExceptionUtils;
 
 import com.jentfoo.file.FileListenerInterface;
@@ -15,6 +22,17 @@ import com.jentfoo.file.FileListenerInterface;
 public class SpellcheckFileListener implements FileListenerInterface {
   private static final String JAVA_EXTENSION = ".java";
   private static final String EMPTY_STR = "";
+  private static final String SEMI_STR = ";";
+  private static final String LEFT_BRACKET_STR = "{";
+  private static final String RIGHT_BRACKET_STR = "}";
+  private static final short MIN_WORD_LENGTH = 4;
+  private static final String VARIABLE_NAME_PATTERN = "[a-zA-Z][a-zA-Z0-9_]*";
+  private static final String DECELERATION_END_PATTERN = " *[=|;]";
+  private static final Pattern PRIMITIVE_DECELERATION_PATTERN = Pattern.compile("(short|int|long|float|double|boolean|char|byte|Short|Integer|Long|Float|Double|Boolean|Char|Byte) " + 
+                                                                                  VARIABLE_NAME_PATTERN + DECELERATION_END_PATTERN);
+  private static final Pattern OBJECT_DECELERATION_PATTERN = Pattern.compile("[A-Z][a-zA-Z0-9]* " + 
+                                                                               VARIABLE_NAME_PATTERN + DECELERATION_END_PATTERN);
+  private static final Pattern VARIABLE_GROUP_PATTERN = Pattern.compile(" (" + VARIABLE_NAME_PATTERN + ")" + DECELERATION_END_PATTERN);
   
   private final AtomicInteger examinedFiles = new AtomicInteger(0);
 
@@ -35,6 +53,9 @@ public class SpellcheckFileListener implements FileListenerInterface {
   }
   
   protected void spellCheckJavaFile(File file) throws IOException {
+    JLanguageTool langTool = new JLanguageTool(new English());
+    langTool.activateDefaultPatternRules();
+    
     BufferedReader br = new BufferedReader(new FileReader(file));
     try {
       int lineCount = 0;
@@ -46,8 +67,14 @@ public class SpellcheckFileListener implements FileListenerInterface {
         currentJavaLine += line;
         if (isEndOfJavaLine(currentJavaLine)) {
           currentJavaLine = minimizeJavaLine(currentJavaLine);
-          if (isDecelerationLine(currentJavaLine)) {
-            analyzeSpelling(currentJavaLine);
+          List<String> toInspectLines = breakApartLine(currentJavaLine);
+          Iterator<String> it = toInspectLines.iterator();
+          while (it.hasNext()) {
+            String inspectLine = it.next();
+            String variable = getVariable(inspectLine);
+            if (variable != null) {
+              analyzeSpelling(langTool, variable);
+            }
           }
           
           currentJavaLine = EMPTY_STR;
@@ -59,7 +86,9 @@ public class SpellcheckFileListener implements FileListenerInterface {
   }
 
   protected static boolean isEndOfJavaLine(String line) {
-    return line.endsWith(";") || line.endsWith("{") || line.endsWith("}");
+    return line.endsWith(SEMI_STR) || 
+             line.endsWith(LEFT_BRACKET_STR) || 
+             line.endsWith(RIGHT_BRACKET_STR);
   }
   
   protected static String minimizeJavaLine(String line) {
@@ -69,32 +98,54 @@ public class SpellcheckFileListener implements FileListenerInterface {
     
     return line;
   }
-  
-  private static final String VARIABLE_NAME_PATTERN = "[a-zA-Z][a-zA-Z0-9_]*";
-  private static final String DECELERATION_END_PATTERN = " *[=|;]";
-  private static final Pattern PRIMITIVE_DECELERATION_PATTERN = Pattern.compile("(short|int|long|float|double|boolean|char|byte|Short|Integer|Long|Float|Double|Boolean|Char|Byte) " + 
-                                                                                  VARIABLE_NAME_PATTERN + DECELERATION_END_PATTERN);
-  private static final Pattern OBJECT_DECELERATION_PATTERN = Pattern.compile("[A-Z][a-zA-Z0-9]* " + 
-                                                                               VARIABLE_NAME_PATTERN + DECELERATION_END_PATTERN);
-  
-  protected static boolean isDecelerationLine(String line) {
-    Matcher m = PRIMITIVE_DECELERATION_PATTERN.matcher(line);
-    if (m.find()) {
-      //System.out.println("prim:" + m.group());
-      return true;
+
+  protected static List<String> breakApartLine(String line) {
+    StringTokenizer st = new StringTokenizer(line, SEMI_STR);
+    List<String> result = new ArrayList<String>(st.countTokens());
+    while (st.hasMoreTokens()) {
+      result.add(st.nextToken() + SEMI_STR);
     }
     
-    m = OBJECT_DECELERATION_PATTERN.matcher(line);
-    if (m.find()) {
-      //System.out.println("obj: " + m.group());
-      return true;
-    }
-    
-    return false;
+    return result;
   }
   
-  protected static void analyzeSpelling(String line) {
-    
+  protected static boolean isPrimitiveDecelerationLine(String line) {
+    Matcher m = PRIMITIVE_DECELERATION_PATTERN.matcher(line);
+    return m.find();
+  }
+  
+  protected static boolean isObjectDecelerationLine(String line) {
+    Matcher m = OBJECT_DECELERATION_PATTERN.matcher(line);
+    return m.find();
+  }
+
+  protected static String getVariable(String inspectLine) {
+    if (isPrimitiveDecelerationLine(inspectLine) || 
+        isObjectDecelerationLine(inspectLine)) {
+      Matcher m = VARIABLE_GROUP_PATTERN.matcher(inspectLine);
+      m.find();
+      return m.group(1);
+    } else {
+      return null;
+    }
+  }
+  
+  protected static void analyzeSpelling(JLanguageTool langTool, 
+                                        String variable) throws IOException {
+    for(String word : variable.split("(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])")) {
+      if (word.length() > MIN_WORD_LENGTH) {
+        List<RuleMatch> matches = langTool.check(word);
+        Iterator<RuleMatch> it = matches.iterator();
+        while (it.hasNext()) {
+          RuleMatch rm = it.next();
+          // TODO output in a different way
+          System.out.println("Potential error at line " +
+              rm.getLine() + ": " + rm.getMessage());
+          System.out.println("Suggested correction: " +
+              rm.getSuggestedReplacements());
+        }
+      }
+    }
   }
 
   public int getExaminedFileCount() {
--- src/test/java/com/jentfoo/spell/SpellcheckFileListenerTest.java
+++ src/test/java/com/jentfoo/spell/SpellcheckFileListenerTest.java
@@ -2,34 +2,68 @@ package com.jentfoo.spell;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
 
 public class SpellcheckFileListenerTest {
   @Test
+  public void breakApartLineTest() {
+    List<String> result;
+    
+    String firstStr = "int foo = 3;";
+    String testStr = firstStr;
+    result = SpellcheckFileListener.breakApartLine(testStr);
+    assertEquals(1, result.size());
+    assertEquals(firstStr, result.get(0));
+    
+    String secondStr = "short bar = 10;";
+    testStr += secondStr;
+    result = SpellcheckFileListener.breakApartLine(testStr);
+    assertEquals(2, result.size());
+    assertEquals(firstStr, result.get(0));
+    assertEquals(secondStr, result.get(1));
+  }
+  
+  @Test
   public void isDecelerationLinePrimitiveTrueTest() {
-    assertTrue(SpellcheckFileListener.isDecelerationLine("short foo = 3;"));
-    assertTrue(SpellcheckFileListener.isDecelerationLine("short foo=3;"));
-    assertTrue(SpellcheckFileListener.isDecelerationLine("short foo=(short)3;"));
-    assertTrue(SpellcheckFileListener.isDecelerationLine("int foo=bar;"));
-    assertTrue(SpellcheckFileListener.isDecelerationLine("Double foo=bar;"));
-    assertTrue(SpellcheckFileListener.isDecelerationLine("short foo;"));
+    assertTrue(SpellcheckFileListener.isPrimitiveDecelerationLine("short foo = 3;"));
+    assertTrue(SpellcheckFileListener.isPrimitiveDecelerationLine("short foo=3;"));
+    assertTrue(SpellcheckFileListener.isPrimitiveDecelerationLine("short foo=(short)3;"));
+    assertTrue(SpellcheckFileListener.isPrimitiveDecelerationLine("int foo=bar;"));
+    assertTrue(SpellcheckFileListener.isPrimitiveDecelerationLine("Double foo=bar;"));
+    assertTrue(SpellcheckFileListener.isPrimitiveDecelerationLine("short foo;"));
   }
   
   @Test
   public void isDecelerationLinePrimitiveFalseTest() {
-    assertFalse(SpellcheckFileListener.isDecelerationLine(""));
-    assertFalse(SpellcheckFileListener.isDecelerationLine("(short)foo;"));
+    assertFalse(SpellcheckFileListener.isPrimitiveDecelerationLine(""));
+    assertFalse(SpellcheckFileListener.isPrimitiveDecelerationLine("(short)foo;"));
   }
   
   @Test
   public void isDecelerationLineObjectTrueTest() {
-    assertTrue(SpellcheckFileListener.isDecelerationLine("FooBar foo;"));
-    assertTrue(SpellcheckFileListener.isDecelerationLine("FooBar foo = null"));
+    assertTrue(SpellcheckFileListener.isObjectDecelerationLine("FooBar foo;"));
+    assertTrue(SpellcheckFileListener.isObjectDecelerationLine("FooBar foo = null"));
   }
   
   @Test
   public void isDecelerationLineObjectFalseTest() {
-    assertFalse(SpellcheckFileListener.isDecelerationLine("FooBar foo"));
-    assertFalse(SpellcheckFileListener.isDecelerationLine("FooBar;"));
+    assertFalse(SpellcheckFileListener.isObjectDecelerationLine("FooBar foo"));
+    assertFalse(SpellcheckFileListener.isObjectDecelerationLine("FooBar;"));
+  }
+  
+  @Test
+  public void getVariableTest() {
+    // primitives
+    assertEquals("foo", SpellcheckFileListener.getVariable("short foo = 3;"));
+    assertEquals("foo", SpellcheckFileListener.getVariable("short foo=3;"));
+    assertEquals("foo", SpellcheckFileListener.getVariable("short foo=(short)3;"));
+    assertEquals("foo", SpellcheckFileListener.getVariable("int foo=bar;"));
+    assertEquals("foo", SpellcheckFileListener.getVariable("Double foo=bar;"));
+    assertEquals("foo", SpellcheckFileListener.getVariable("short foo;"));
+    // objects
+    assertEquals("foo", SpellcheckFileListener.getVariable("FooBar foo;"));
+    assertEquals("foo", SpellcheckFileListener.getVariable("FooBar foo = null"));
   }
 }
